bf735bb6ad76a94678e34de18b9517c373c600a3
mrop007 <mukherjee.prithwish4816367@gmail.com>
2022-03-10 13:43:26 +0530

    Exercise1 8th sol

--- src/lectures/homework/answers/exercise1.java
+++ src/lectures/homework/answers/exercise1.java
@@ -84,7 +84,7 @@ public static void counting() {
     }
     
     public static void main(String[]args){
-        int n = power(3,3)*3;
+        int n = power(3,3)*3;//added the last multiply cz the func starts from the secon latst num example for pow 3 it will start from pow 2
         System.out.println(n);
     }
 
