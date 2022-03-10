d8a24c7e94a55724c894d143172b359b7ea7d7ce
mrop007 <mukherjee.prithwish4816367@gmail.com>
2022-03-10 13:00:52 +0530

    Exercise1 7th sol

--- src/lectures/homework/answers/exercise1.java
+++ src/lectures/homework/answers/exercise1.java
@@ -1,56 +1,82 @@
 package lectures.homework.answers;
 import java.util.*;
 public class exercise1 {
-    public static double average(int marks1,int marks2,int marks3){
-        int avg = (marks1+marks2+marks3)/3;
+    public static double average(int marks1, int marks2, int marks3) {
+        int avg = (marks1 + marks2 + marks3) / 3;
         return avg;
     }
-    public static int sumallodds(int n){
+
+    public static int sumallodds(int n) {
         int sum = 0;
-        for(int i = 1;i<=n;i++){
-            if(i%2==0){
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
                 continue;
-            }
-            else{
+            } else {
                 sum += i;
             }
         }
         return sum;
     }
-    public static int greaternum(int n,int j){
+
+    public static int greaternum(int n, int j) {
         int greaternum = n;
-        if(n > j){
+        if (n > j) {
             greaternum = n;
-        }
-        else if(n<j){
+        } else if (n < j) {
             greaternum = j;
         }
         return greaternum;
     }
-    public static long circumfrence(int rad){
+
+    public static long circumfrence(int rad) {
         double pi = 3.14;
         return (long) ((rad * 2) * pi);
     }
-    public static void eligibletoVote(int age){
-        if(age >18){
+
+    public static void eligibletoVote(int age) {
+        if (age > 18) {
             System.out.println("eligible to vote");
-        }
-        else{
+        } else {
             System.out.println("Not eligible to vote");
         }
     }
-    public static void infloop(int i){
-        do{
+
+    public static void infloop(int i) {
+        do {
             System.out.println("I use apna colleage for learning java");
         }
         while (i != 0);
     }
-    public static void counting(){
 
+public static void counting() {
+        Scanner input = new Scanner(System.in);
+        int posnum = 0;
+        int negnum = 0;
+        int zero = 0;
+       System.out.println("Enter 1 to continue and 0 to end :) ");
+       int inputchecker = input.nextInt();
+        while(inputchecker == 1) {
+            System.out.println("Enter your number : ");
+            int number = input.nextInt();
+            if(number > 0) {
+                posnum++;
+            } else if(number < 0) {
+                negnum++;
+            } else {
+                zero++;
+            }
+
+            System.out.println("Press 1 to continue & 0 to stop");
+            inputchecker = input.nextInt();
+        }
+
+        System.out.println("Positives : "+ posnum);
+        System.out.println("Negatives : "+ negnum);
+        System.out.println("Zeros : "+ zero);
     }
+    
     public static void main(String[]args){
-        long mera = circumfrence(9);
-        System.out.println(mera);
+        counting();
     }
 
 }
